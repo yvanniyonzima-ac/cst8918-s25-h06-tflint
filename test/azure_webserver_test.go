b3cd8e7ae1eecb4ac3df77d9bc77a6bfa5afae61
yvanniyonzima-ac <niyo0041@algonquinlive.com>
2025-06-14 16:27:52 -0400

    added tflint all presents and fixed warnings

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "bd7bd383-5df6-4911-a820-52a7dc9cdb5b"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": "niyo0041",
 		},
 	}
 
